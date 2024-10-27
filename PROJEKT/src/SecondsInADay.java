092b9ebc40dce16f0d5f4955af86da6b571737c7
AdamKatrenic <adamkatrenic@gmail.com>
2024-10-28 00:49:15 +0100

    More Variables exercises, temp, concat

---
+++ PROJEKT/src/SecondsInADay.java
@@ -0,0 +1,31 @@
+public class SecondsInADay {
+    //public class SecondsInADay {
+    //  public static void main(String[] args) {
+    //    int currentHours = 14;
+    //    int currentMinutes = 34;
+    //    int currentSeconds = 42;
+    //
+    //    // Write a program that prints the remaining seconds (as an integer) from a
+    //    // day if the current time is represented by the variables above
+    //  }
+    //}
+
+    public static void main(String[] args) {
+         int currentHours = 14;
+         int currentMinutes = 34;
+         int currentSeconds = 42;
+
+         int DayInSeconds = 24*60*60;
+
+
+         //int SecondsInADay = 24 * 60 * 60;
+         //int SecondsInAMinute = 60 * 60;
+        // int SecondsInASecond= 60;
+
+
+        System.out.println("Seconds Remaining: " + (DayInSeconds - (currentHours*60*60)-(currentMinutes*60)-(currentSeconds)));
+
+
+
+    }
+}
---
+++ PROJEKT/src/Swap.java
@@ -0,0 +1,26 @@
+public class Swap {
+    //public class Swap {
+    //  public static void main(String[] args) {
+    //    // Swap the values of the variables without changing the following 2 lines of code
+    //    int a = 123;
+    //    int b = 526;
+    //
+    //    // Write your code here:
+    //
+    //    System.out.println(a);
+    //    System.out.println(b);
+    //  }
+    //}
+
+    public static void main(String[] args) {
+        int a = 123;
+        int b = 526;
+        int temp = a; //riadok uklada hodnotu a do temporary
+
+        a = b; //tento riadok urciuje hodnotu acka becku
+        b = temp; //priradzuje hotnotu temp (originalnemu acku) do becka
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
