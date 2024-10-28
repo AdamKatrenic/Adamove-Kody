fbcfe56f0538cad66a7cdc2095e136052cc4dad5
AdamKatrenic <adamkatrenic@gmail.com>
2024-10-29 00:15:18 +0100

    scanner, VariableMutation, boolean

---
+++ PROJEKT/src/Scan.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Scan {
+
+    public static void main(String[] args) {
+        Scanner Scan1 = new Scanner(System.in);
+        System.out.println("Zadajte vek :");
+        int vek = Scan1.nextInt();
+
+        System.out.println("Vas vek :"+ vek);
+
+
+
+    }
+}
